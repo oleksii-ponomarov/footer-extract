acb56d3232dd8d5ba876bd1143b0ad418389e376
oleksii-ponomarov <rocknrollerr@gmail.com>
2020-10-26 14:14:18 +0200

    Custom url ability added

--- index.js
+++ index.js
@@ -3,80 +3,77 @@ const $ = require('cheerio');
 const fs = require('fs-extra');
 const prettier = require('prettier');
 const uncss = require('uncss');
+const striptags = require('striptags');
 
-const url = 'http://grammarly.com';
+const url = process.argv[2] || 'http://grammarly.com';
+const normalizedUrl = url.startsWith('http://') ? url : `http://${url}`;
 
 (async () => {
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+  console.log(`Scrapping ${normalizedUrl} ...\n`);
 
-    let css;
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    page.on('response', async response => {
-      if (response.request().resourceType() === 'stylesheet') {
-        css = await response.text();
-        return
-      }
-    });
+  let css = '';
 
-    await page.goto(url);
-
-    const content = await page.content();
-
-    if (content) {
-      console.log('HTML obtained');
-    } else {
-      console.log('❌ Error: No HTML found!');
-      return
+  page.on('response', async response => {
+    if (response.request().resourceType() === 'stylesheet') {
+      css = css + await response.text();
     }
+  });
 
-    const footer = $('footer', content);
+  const goto = await page.goto(normalizedUrl).catch(() => {
+    console.log('❌', 'Error: invalid URL');
+  });
 
-    if (content) {
-      console.log('Footer found');
-    } else {
-      console.log('❌ Error: No footer found!');
-      return
-    }
-
-    const footerClass = footer.attr('class');
-    const footerHTML = footer.html();
-    const prettyFooterHTML = prettier.format(`<footer class="${footerClass}">${footerHTML}</footer>`, {
-      parser: 'html',
-      htmlWhitespaceSensitivity: 'strict'
-    });
-
-    fs.outputFile(
-      './output/footer.html',
-      prettyFooterHTML,
-      (e) => {
-        if (e) console.log('❌', e);
-        console.log('✅ Footer HTML extracted and saved')
-      }
-    );
-
-    if (css) {
-      console.log('Styles obtained');
-      uncss(prettyFooterHTML, {
-        raw: prettier.format(css, {
-          parser: 'css'
-        })
-      }, (error, output) => {
-        if (error) console.log(error);
-        fs.outputFile('./output/footer.css', output, (e) => {
-          if (e) console.log('❌', e);
-          console.log('✅ Footer CSS extracted and saved')
-        });
-      });
-    } else {
-      console.log('❌ Error: No CSS file found!');
-      return
-    }
-
+  if (goto === undefined) {
     await browser.close();
-  } catch (e) {
-    console.log('❌', e);
     return
   }
+
+  const content = await page.content();
+  const footer = $('footer', content);
+
+  if (footer.length !== 0) {
+    console.log('Footer found');
+  } else {
+    console.log('❌ Error: No footer found!');
+    await browser.close();
+    return
+  }
+
+  const footerClass = footer.attr('class');
+  const footerHTML = footer.html();
+  const prettyFooterHTML = prettier.format(`<footer class="${footerClass}">${footerHTML}</footer>`, {
+    parser: 'html',
+    htmlWhitespaceSensitivity: 'strict'
+  });
+
+  if (footerHTML) fs.outputFile(
+    './output/footer.html',
+    prettyFooterHTML,
+    (e) => {
+      if (e) console.log('❌', e);
+      console.log('✅ Footer HTML extracted and saved')
+    }
+  );
+
+  if (css) {
+    console.log('Styles obtained\n');
+    uncss(prettyFooterHTML, {
+      raw: prettier.format(striptags(css), {
+        parser: 'css'
+      })
+    }, (error, output) => {
+      if (error) console.log(error);
+      fs.outputFile('./output/footer.css', output, (e) => {
+        if (e) console.log('❌', e);
+        console.log('✅ Footer CSS extracted and saved')
+      });
+    });
+  } else {
+    console.log('❌ Error: No CSS file found!');
+  }
+
+  await browser.close();
 })();
